be5dbd16d09e2615f5c0e22858f73a8a9cd5c308
Michal Hrbek <michal@dee.cz>
2025-10-20 20:01:29 +0200

    Shortened notice

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 #!/usr/bin/env python3
-# Copyright 2021 Seek Thermal Inc.
-#
 # Original author: Michael S. Mead <mmead@thermal.com>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Modified from original which can be found at https://github.com/seekthermal/seekcamera-python/blob/3b056f94225a17190d4fd78bb59a690baa5946a9/examples/seekcamera-opencv.py
+# Licensed under https://www.apache.org/licenses/LICENSE-2.0
+# Modified from an example which can be found at https://github.com/seekthermal/seekcamera-python/blob/3b056f94225a17190d4fd78bb59a690baa5946a9/examples/seekcamera-opencv.py
 
 from threading import Condition
 from typing import Optional
