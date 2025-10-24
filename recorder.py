9f4ce34c1c8c449390ffa80ffb65c6abbe3f6d19
Michal Hrbek <michal@dee.cz>
2025-10-24 16:59:41 +0200

    Use pygame for recording

--- recorder.py
+++ recorder.py
@@ -1,18 +1,11 @@
-#!/usr/bin/env python3
-# Original author: Michael S. Mead <mmead@thermal.com>
-# Licensed under https://www.apache.org/licenses/LICENSE-2.0
-# Modified from an example which can be found at https://github.com/seekthermal/seekcamera-python/blob/3b056f94225a17190d4fd78bb59a690baa5946a9/examples/seekcamera-opencv.py
-
+import time
 from threading import Condition
 from typing import Optional
 from concurrent.futures import ProcessPoolExecutor
 
 import cv2
-import imageutils, exrutils
 import numpy as np
-import time
-from videocapture import BufferlessVideoCapture
-
+import pygame as pg
 from seekcamera import (
     SeekCameraIOType,
     SeekCameraColorPalette,
@@ -24,6 +17,10 @@ from seekcamera import (
     SeekCamera,
     SeekFrame,
 )
+
+import exrutils
+import player
+from videocapture import BufferlessVideoCapture
 
 
 class Renderer:
@@ -68,49 +65,61 @@ def on_event(camera: SeekCamera, event_type: SeekCameraManagerEvent, event_statu
     elif event_type == SeekCameraManagerEvent.READY_TO_PAIR:
         return
 
+
 def bgr_white_hot(img: np.ndarray) -> np.ndarray:
     return cv2.cvtColor((img*255.0).astype(np.uint8), cv2.COLOR_GRAY2BGR)
 
-def main():
-    window_name = "Video recorder"
-    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-    
+
+def loop():
     cam = BufferlessVideoCapture(0)
     executor = ProcessPoolExecutor() # Image export is done in another process
     frame_counter = 1
+    player.update_images(np.random.rand(480,640,3), np.linspace(20.0, 40.0, 240*320, dtype=np.float32).reshape(240, 320, 1), "No data found! Showing example data") # Generate random rgb data and temperature values from 20C to 40C
+
+    screen = pg.display.set_mode((1280, 720), pg.RESIZABLE)
+    clock = pg.time.Clock()
+    running = True
 
     with SeekCameraManager(SeekCameraIOType.USB) as manager:
         renderer = Renderer()
         manager.register_event_callback(on_event, renderer)
 
-        while True:
+        while running:
             with renderer.frame_condition:
                 if renderer.frame_condition.wait(150.0 / 1000.0):
                     print("Render")
-                    # Render the image to the window.
                     bgr_frame = cam.read()
-                    thermal_frame_processed = cv2.resize(bgr_white_hot(imageutils.normalize(renderer.frame.data)), (bgr_frame.shape[1],bgr_frame.shape[0]))
-                    cv2.imshow(window_name, np.concatenate((bgr_frame,thermal_frame_processed), axis=1))
-                    
-                    # Export image
                     rgb_frame = cv2.cvtColor(bgr_frame, cv2.COLOR_BGR2RGB).astype('float16')/255.0
-                    future = executor.submit(exrutils.write_dual_image, rgb_frame, renderer.frame.data.astype('float32'), f"out/{int(time.time()*1000)}:{frame_counter:04}.exr")
+                    thermal_data = renderer.frame.data.astype('float32')
+                    file_name = f"out/{int(time.time()*1000)}:{frame_counter:04}.exr"
+                    future = executor.submit(exrutils.write_dual_image, rgb_frame, thermal_data, file_name)
+                    player.update_images(rgb_frame, thermal_data, file_name)
                     frame_counter += 1
 
-            # Process key events.
-            key = cv2.waitKey(1)
-            if key == ord("q"):
-                break
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = False
+                if event.type == pg.KEYUP:
+                    if event.key == pg.K_q:
+                        running = False
+                for i in player.elements:
+                    i.handle_event(event)
 
-            # Check if the window has been closed manually.
-            if not cv2.getWindowProperty(window_name, cv2.WND_PROP_VISIBLE):
-                break
+            for i in player.elements:
+                i.tick()
 
-    print("Quitting")
-    cv2.destroyWindow(window_name)
+            screen.fill("purple")
+
+            for i in player.elements:
+                i.render(screen)
+            
+            pg.display.flip()
+
+            clock.tick(60)
+
     cam.close()
-    executor.shutdown(wait=True)
+    pg.quit()
 
 
 if __name__ == "__main__":
-    main()
+    loop()
