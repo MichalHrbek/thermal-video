49e6512b18a9892227244616944df03c2b0ee0d9
Michal Hrbek <michal@dee.cz>
2025-10-28 20:18:28 +0100

    Default config

--- config.py
+++ config.py
@@ -2,15 +2,25 @@ from configparser import ConfigParser
 from pathlib import Path
 
 DEFAULTS = {
-    # Recorder
-    "camera": 0,
-    "auto_record": False,
-    "write_path": "out/",
-    # Player
-    "auto_play": False,
-    "read_path": "out/*.exr",
-    "color_pallete": 0,
+    "recorder": {
+        "camera": 0,
+        "auto_record": False,
+        "write_path": "out/",
+    },
+    "player": {
+        "auto_play": False,
+        "read_path": "out/*.exr",
+        "color_pallete": 0,
+    },
 }
 
-config = ConfigParser(defaults=DEFAULTS)
+config = ConfigParser()
 config.read(Path(__file__).resolve().parent / "config.ini")
+
+for i in DEFAULTS:
+    if not config.has_section(i):
+        config.add_section(i) 
+    
+    for j in DEFAULTS[i]:
+        if j not in config[i]:
+            config[i][j] = str(DEFAULTS[i][j])
